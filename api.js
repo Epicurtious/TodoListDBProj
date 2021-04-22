9bc8df70c4783295daea63d517bbb8b54d24da90
David-Hatcher <48420049+David-Hatcher@users.noreply.github.com>
2021-04-21 21:52:57 -0400

    Written by Curtis Goodwin
    Not

--- api.js
+++ api.js
@@ -3,13 +3,18 @@ var cors = require('cors');
 var mysql = require('mysql');
 var app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
 
-var con = mysql.createConnection({
-  host: 'us-cdbr-east-03.cleardb.com',
-  user: 'b544be9947b187',
-  password: '337d07ea',
-  database: "heroku_9c6c8776a0ac640"
-});
+const getConnection = () => {
+  let con = mysql.createConnection({
+    host: 'us-cdbr-east-03.cleardb.com',
+    user: 'b544be9947b187',
+    password: '337d07ea',
+    database: "heroku_9c6c8776a0ac640"
+  });
+  return con
+}
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
@@ -17,23 +22,45 @@ app.listen(5000, () => {
 
 //get all departments
 app.get('/queries/getDepartments', (req,res,next) => {
+  let con = getConnection();
+  let output = {}
   con.query("SELECT * FROM department", function (err, result, fields) {
     if (err) throw err;
-    res.json(result);
+    result.forEach(element => {
+      output[element['deptId']] = {
+        "deptName": element["deptName"],
+        "deptBld": element["deptBld"]
+      }
+    })
+    res.json(output);
   })
+  con.end()
 });
 
 //get all employees
 app.get('/queries/getEmployees', (req,res,next) => {
-  con.query("SELECT * FROM employees", function (err, result, fields) {
+  let con = getConnection()
+  let output = {};
+  con.query("SELECT * FROM employees e, department d, emp_dept ed WHERE e.empId = ed.empId AND d.deptId = ed.deptId", function (err, result, fields) {
     if (err) throw err;
-    res.json(result);
+    result.forEach(element => {
+      let name = `${element["fname"]} ${element["lname"]}`
+      output[element['empId']] = {
+        "name": name,
+        "phone": element["phone"],
+        "deptId": element["deptId"],
+        "deptName": element["deptName"],
+        "deptBld": element["deptBld"]
+      }
+    });
+    res.json(output);
   })
+  con.end();
 });
 
 //get tasks by department
 app.get('/queries/getTasksByDepartment', (req,res,next) => {
-  con.query("SELECT * FROM department d, dept_task dt, task t WHERE d.deptId = dt.deptId AND t.taskId = dt.taskId", function (err, result, fields) {
+  getConnection.query("SELECT * FROM department d, dept_task dt, task t WHERE d.deptId = dt.deptId AND t.taskId = dt.taskId", function (err, result, fields) {
     if (err) throw err;
     res.json(result);
   })
@@ -41,7 +68,7 @@ app.get('/queries/getTasksByDepartment', (req,res,next) => {
 
 //get tasks by user
 app.get('/queries/getTasksByUser', (req,res,next) => {
-  con.query("SELECT * FROM employee e, emp_task et, task t WHERE e.empId = et.empId AND t.taskId = et.taskId", function (err, result, fields) {
+  getConnection.query("SELECT * FROM employee e, emp_task et, task t WHERE e.empId = et.empId AND t.taskId = et.taskId", function (err, result, fields) {
     if (err) throw err;
     res.json(result);
   })
@@ -49,23 +76,23 @@ app.get('/queries/getTasksByUser', (req,res,next) => {
 
 //get all tasks (task should have the department, the employee, and the task information on it)
 app.get('/queries/getTasksByUser', (req,res,next) => {
-  con.query("SELECT * FROM department d, employee e, emp_task et, task t WHERE d.deptId = dt.deptId AND e.empId = et.empId AND t.taskId = et.taskId AND t.taskId = dt.taskId", function (err, result, fields) {
+  getConnection.query("SELECT * FROM department d, employee e, emp_task et, task t WHERE d.deptId = dt.deptId AND e.empId = et.empId AND t.taskId = et.taskId AND t.taskId = dt.taskId", function (err, result, fields) {
     if (err) throw err;
     res.json(result);
   })
 });
 
 //get task count for all departments
-app.get('/queries/getCountOfUser', (req,res,next) => {
-  con.query("SELECT COUNT(taskId) FROM dept_task", function (err,result,field) {
+app.get('/queries/getTotalTaskCount', (req,res,next) => {
+  getConnection.query("SELECT COUNT(taskId) FROM dept_task", function (err,result,field) {
     if(err) throw err;
     res.json(result);
   })
 })
 
 //get task count of department
-app.get('/queries/getCountOfDepartment', (req,res,next) => {
-  con.query("SELECT COUNT(dt.taskId) AS cnt FROM dept_task dt GROUP BY dt.deptId", function (err,result,field) {
+app.get('/queries/getTaskCountOfDepartment', (req,res,next) => {
+  getConnection.query("SELECT d.deptId d.deptName COUNT(dt.taskId) AS cnt FROM dept_task dt, department d WHERE d.deptd = dt.deptId GROUP BY dt.deptId", function (err,result,field) {
     if(err) throw err;
     res.json(result);
   })
@@ -73,32 +100,51 @@ app.get('/queries/getCountOfDepartment', (req,res,next) => {
 
 //get task count of user
 app.get('/queries/getCountOfUser', (req,res,next) => {
-  con.query("SELECT COUNT(et.taskId) AS cnt FROM emp_task et GROUP BY et.empId", function (err,result,field) {
+  getConnection.query("SELECT COUNT(et.taskId) AS cnt FROM emp_task et GROUP BY et.empId", function (err,result,field) {
     if(err) throw err;
     res.json(result);
   })
 })
 
 //get task count for all users
-app.get('/queries/getCountOfUser', (req,res,next) => {
-  con.query("SELECT COUNT(taskId) FROM emp_task", function (err,result,field) {
+app.get('/queries/getCountOfAllUser', (req,res,next) => {
+  getConnection.query("SELECT COUNT(taskId) FROM emp_task", function (err,result,field) {
     if(err) throw err;
     res.json(result);
   })
 })
 
 
+
+//get task Id
+app.get('/queries/getTaskId', (req,res) => {
+  let taskId = null;
+  let con = getConnection();
+  con.query("CALL maxTaskId()", function (err, result, fields) {
+    if (err) throw err;
+    taskId = parseInt(result[0][0].taskId);
+    taskId = (++taskId).toString();
+    res.json({"taskId" : taskId})
+  })
+  con.end()
+})
+
 //add a task to the db
 app.post('/posts/addTask', (req,res) => {
-  let body = res.body;
-  let taskId = null
-  let sql = "INSERT INTO task (taskId, dueDate, createDate, status, description) VALUES (" + taskId + "," + body.dueDate + "," + body.createDate + "," + body.status + "," + body.description + ")"
-  con.query("CALL max_taskId(@output); select @output", function (err, result, fields) {
+  let body = req.body;
+  let sqlTask = `INSERT INTO task (taskId, dueDate, createDate, status, priority, description) VALUES ('${body.taskId}', '${body.dueDate}', '${body.createDate}', '${body.status}', '${body.priority}', '${body.description}')`
+  let sqlDept = `INSERT INTO dept_task (taskId, deptId) VALUES ('${body.taskId}', '${body.deptId}')`
+  let sqlEmp = `INSERT INTO emp_task (taskId, empId) VALUES ('${body.taskId}', '${body.empId}')`
+  let con = getConnection()
+  con.query(sqlTask, function (err, result, fields) {
     if (err) throw err;
-    taskId = result[1][0]['@output']
   })
-  con.query(sql, function (err, result, fields) {
+  con.query(sqlDept, function (err, result, fields) {
     if (err) throw err;
-    res.json(result);
   })
+  con.query(sqlEmp, function (err, result, fields) {
+    if (err) throw err;
+  })
+  res.json({task:"accepted"});
+  con.end();
 });
