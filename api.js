c5f315484035ceb8e1eab650a495d99cc20ae41f
Curtis Goodwin <curtisgoodwin117@yahoo.com>
2021-04-25 18:41:25 -0400

    Added UpdateTask
    UpdateTask is tested

--- api.js
+++ api.js
@@ -23,6 +23,8 @@ app.listen(5000, () => {
 
 //get all departments
 app.get('/queries/getDepartments', (req,res,next) => {
+  let d = new Date();
+  console.log(`Fulfilling get departments request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let con = getConnection();
   let output = {}
   con.query("SELECT * FROM department", function (err, result, fields) {
@@ -40,6 +42,8 @@ app.get('/queries/getDepartments', (req,res,next) => {
 
 //get all employees
 app.get('/queries/getEmployees', (req,res,next) => {
+  let d = new Date();
+  console.log(`Fulfilling get employees request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let con = getConnection()
   let output = {};
   con.query("SELECT * FROM employees e, department d, emp_dept ed WHERE e.empId = ed.empId AND d.deptId = ed.deptId", function (err, result, fields) {
@@ -91,6 +95,8 @@ app.get('/queries/getTasksByUser', (req,res,next) => {
 
 //gets the task count of each status
 app.get('/queries/getTaskCountByStatus', (req,res,next) => {
+  let d = new Date();
+  console.log(`Fulfilling get task count request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let con = getConnection();
   con.query(`SELECT t.taskStatus, COUNT(t.taskId) as taskCount FROM task t WHERE t.taskStatus != 'Closed' GROUP BY t.taskStatus;`, function (err,result,field) {
     if (err) throw err;
@@ -101,6 +107,8 @@ app.get('/queries/getTaskCountByStatus', (req,res,next) => {
 
 //get task count of each department
 app.get('/queries/getTaskCountOfDepartment', (req,res,next) => {
+  let d = new Date();
+  console.log(`Fulfilling get task count of department request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let con = getConnection();
   con.query(`SELECT d.deptId, d.deptName, COUNT(dt.taskId) as taskCount FROM dept_task dt, department d WHERE d.deptId = dt.deptId GROUP BY d.deptId, d.deptName;`, function (err,result,field) {
     if(err) throw err;
@@ -111,6 +119,8 @@ app.get('/queries/getTaskCountOfDepartment', (req,res,next) => {
 
 //get task count of each user
 app.get('/queries/getTaskCountOfUser', (req,res,next) => {
+  let d = new Date();
+  console.log(`Fulfilling get task count of user request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let con = getConnection();
   con.query(`SELECT e.empId, CONCAT(e.fname,' ',e.lname) AS name, COUNT(et.taskId) as taskCount FROM employees e, emp_task et, task t WHERE e.empId = et.empId AND et.taskId = t.taskId GROUP BY e.empId, e.fname, e.lname`, function (err,result,field) {
     if(err) throw err;
@@ -122,6 +132,8 @@ app.get('/queries/getTaskCountOfUser', (req,res,next) => {
 
 //get task Id
 app.get('/queries/getTaskId', (req,res) => {
+  let d = new Date();
+  console.log(`Fulfilling get task ID request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let taskId = null;
   let con = getConnection();
   con.query("CALL maxTaskId()", function (err, result, fields) {
@@ -135,6 +147,8 @@ app.get('/queries/getTaskId', (req,res) => {
 
 //add a task to the db
 app.post('/posts/addTask', (req,res) => {
+  let d = new Date();
+  console.log(`Fulfilling post task request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
   let body = req.body;
   let sqlTask = `INSERT INTO task (taskTitle, taskId, dueDate, createDate, taskStatus, priority, description) VALUES ('${body.taskTitle}','${body.taskId}', '${body.dueDate}', '${body.createDate}', '${body.status}', '${body.priority}', '${body.description}')`
   let sqlDept = `INSERT INTO dept_task (taskId, deptId) VALUES ('${body.taskId}', '${body.deptId}')`
@@ -156,7 +170,24 @@ app.post('/posts/addTask', (req,res) => {
 
 //update a task in the DB
 app.post('/posts/updateTask', (req, res) => {
-  //do stuff
+  let d = new Date();
+  console.log(`Fulfilling update tasks request at ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
+  let body = req.body;
+  let sqlTaskUpdate = `UPDATE task SET taskTitle='${body.taskTitle}', dueDate='${body.dueDate}', createDate='${body.createDate}', taskStatus='${body.status}', priority='${body.level}', description='${body.description}' WHERE taskId='${body.taskId}'`
+  let sqlEmpTaskUpdate = `UPDATE emp_task SET empId='${body.empId}' WHERE taskId='${body.taskId}'`
+  let sqlDeptTaskUpdate = `UPDATE dept_task SET deptId='${body.deptId}' WHERE taskId='${body.taskId}'`
+  let con = getConnection()
+  con.query(sqlTaskUpdate, function (err, result, fields) {
+    if (err) throw err;
+  })
+  con.query(sqlEmpTaskUpdate, function (err, result, fields) {
+    if (err) throw err;
+  })
+  con.query(sqlDeptTaskUpdate, function (err, result, fields) {
+    if (err) throw err;
+  })
+  res.json({task:"accepted"});
+  // con.end();
 })
 
 
